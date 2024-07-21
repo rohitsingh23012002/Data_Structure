6eb24ba61072b79308f1e9931bcaf22346e1db94
ROHIT SINGH <85890547+rohitsingh23012002@users.noreply.github.com>
2024-07-21 20:14:41 +0530

    stack using arraylist

---
+++ practice/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float e = 6.6f;
+        e++;
+        System.out.println(e);
+        char a = 'u';
+        a++;
+        System.out.println(a);
+    }
+}
